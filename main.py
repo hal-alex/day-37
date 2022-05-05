bdc1871418f74db6c6141b6b8602e020f943bfcf
hal-alex <102769562+hal-alex@users.noreply.github.com>
2022-05-06 00:13:17 +0100

    Adding puts and delete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 TOKEN = ""
 USERNAME = "alex111"
@@ -34,11 +35,25 @@ headers = {
 
 create_point_endpoint = f"{graph_endpoint}/{graph_config['id']}"
 
+today_date = datetime(year=2022, month=5, day=2)
+
+current_date = today_date.strftime("%Y%m%d")
+
 point_config = {
-    "date": "20220504",
-    "quantity": "2.7",
+    "date": current_date,
+    "quantity": "2.6",
 }
 
-response = requests.post(url=create_point_endpoint, headers=headers, json=point_config)
+update_point_endpoint = f"{create_point_endpoint}/{current_date}"
+
+modify_point_config = {
+    "quantity": "2.5",
+}
+
+# response = requests.put(url=update_point_endpoint, headers=headers, json=modify_point_config)
+#
+# print(response.text)
+
+response = requests.delete(url=update_point_endpoint, headers=headers)
 
 print(response.text)
